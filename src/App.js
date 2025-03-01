5fcaa6f37e3f8744652743967cf3395a0fd4e8f8
wattanawipa <minn7714@gmail.com>
2025-03-01 15:41:27 +0700

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+
+
 function App() {
  return (
-<div className="app-bg">
-<h1>Portfolio</h1>
-</div>
+    <div style={{textAlign:"center", color: "#black"}}>
+    <h1>Portfolio</h1>
+    <h2>Wattanawipa Nakhawtom</h2>
+    </div>
+
  );
 }
+
 export default App;
